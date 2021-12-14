1d15e9fcd8288720a8d3d2ffedef7c8887c12e69
kalleakerblom <kalleakerblom@gmail.com>
2021-12-14 22:36:48 +0100

    update day01

--- src/day01.rs
+++ src/day01.rs
@@ -1,11 +1,18 @@
-fn count_increasing(input: &[u32]) -> usize {
-    input.windows(2).filter(|w| w[0] < w[1]).count()
+use itertools::Itertools;
+
+fn part_1(input: &str) -> usize {
+    let depths = input.lines().map(|l| l.parse().unwrap());
+    depths
+        .tuple_windows()
+        .filter(|(a, b): &(u32, u32)| a < b)
+        .count()
 }
 
-fn count_increasing_window_sums(input: &[u32], window_size: usize) -> usize {
-    input
-        .windows(window_size)
-        .map(|w| w.iter().sum())
+fn part_2(input: &str) -> usize {
+    let depths = input.lines().map(|l| l.parse().unwrap());
+    depths
+        .tuple_windows()
+        .map(|(a, b, c): (u32, u32, u32)| a + b + c)
         .fold((0, u32::MAX), |(count, prev_sum), sum| {
             if prev_sum < sum {
                 (count + 1, sum)
@@ -15,33 +22,31 @@ fn count_increasing_window_sums(input: &[u32], window_size: usize) -> usize {
         })
         .0
 }
-
 #[cfg(test)]
 mod tests {
-    use crate::day01::{count_increasing, count_increasing_window_sums};
+    use crate::day01::{part_1, part_2};
     use std::fs;
     #[test]
     fn example01_part1() {
         let input = fs::read_to_string("input/example01").unwrap();
-        let depths: Vec<u32> = input.lines().map(|l| l.parse().unwrap()).collect();
-        assert_eq!(count_increasing(&depths), 7);
+
+        assert_eq!(part_1(&input), 7);
     }
     #[test]
     fn day01_part1() {
         let input = fs::read_to_string("input/day01").unwrap();
-        let depths: Vec<u32> = input.lines().map(|l| l.parse().unwrap()).collect();
-        assert_eq!(count_increasing(&depths), 1292);
+        assert_eq!(part_1(&input), 1292);
     }
     #[test]
     fn example01_part2() {
         let input = fs::read_to_string("input/example01").unwrap();
-        let depths: Vec<u32> = input.lines().map(|l| l.parse().unwrap()).collect();
-        assert_eq!(count_increasing_window_sums(&depths, 3), 5);
+
+        assert_eq!(part_2(&input), 5);
     }
     #[test]
     fn day01_part2() {
         let input = fs::read_to_string("input/day01").unwrap();
-        let depths: Vec<u32> = input.lines().map(|l| l.parse().unwrap()).collect();
-        assert_eq!(count_increasing_window_sums(&depths, 3), 1262);
+
+        assert_eq!(part_2(&input), 1262);
     }
 }
