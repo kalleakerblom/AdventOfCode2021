4c7b746964587c2c23dde9908c6e6cf84b6aabfb
kalleakerblom <kalleakerblom@gmail.com>
2021-12-15 21:43:16 +0100

    Update day15

--- src/day15.rs
+++ src/day15.rs
@@ -4,21 +4,20 @@ use std::collections::{BinaryHeap, HashMap};
 type RiskMap = Vec<Vec<u32>>;
 type Pos = (usize, usize);
 
-fn get_neighbors((x, y): Pos, max_x: usize, max_y: usize) -> Vec<Pos> {
+fn get_neighbors((x, y): Pos, max_x: usize, max_y: usize) -> impl Iterator<Item = Pos> {
     let (x, y, max_x, max_y) = (x as i64, y as i64, max_x as i64, max_y as i64);
     let neighbors = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)];
     neighbors
-        .iter()
-        .filter(|&&(nx, ny)| 0 <= nx && nx <= max_x && 0 <= ny && ny <= max_y)
-        .map(|(nx, ny)| (*nx as usize, *ny as usize))
-        .collect()
+        .into_iter()
+        .filter(move |&(nx, ny)| 0 <= nx && nx <= max_x && 0 <= ny && ny <= max_y)
+        .map(|(nx, ny)| (nx as usize, ny as usize))
 }
 
 fn min_risk_path(risk_map: &RiskMap) -> u32 {
     let end = (risk_map[0].len() - 1, risk_map.len() - 1);
-    let mut frontier = BinaryHeap::new();
-    frontier.push(Reverse((0, (0, 0)))); // Need Reverse to get min-heap
-    let mut min_total_risk_map = HashMap::new();
+    let mut frontier = BinaryHeap::<Reverse<(u32, Pos)>>::new(); // Need Reverse to get min-heap
+    frontier.push(Reverse((0, (0, 0))));
+    let mut min_path_risk_map = HashMap::new();
     while let Some(Reverse((curr_risk, curr_pos))) = frontier.pop() {
         if curr_pos == end {
             return curr_risk;
@@ -26,9 +25,9 @@ fn min_risk_path(risk_map: &RiskMap) -> u32 {
         for neighbor_pos in get_neighbors(curr_pos, end.0, end.1) {
             let (nx, ny) = neighbor_pos;
             let new_risk = curr_risk + risk_map[ny][nx];
-            let min_total = min_total_risk_map.entry((nx, ny)).or_insert(u32::MAX);
-            if new_risk < *min_total {
-                *min_total = new_risk;
+            let min_path_risk = min_path_risk_map.entry(neighbor_pos).or_insert(u32::MAX);
+            if new_risk < *min_path_risk {
+                *min_path_risk = new_risk;
                 frontier.push(Reverse((new_risk, neighbor_pos)));
             }
         }
@@ -46,12 +45,11 @@ fn build_full_map(map: &RiskMap) -> RiskMap {
             let bonus_risk = x / small_width + y / small_height;
             let small_x = x % small_width;
             let small_y = y % small_height;
-            let new_risk = bonus_risk as u32 + map[small_y][small_x];
-            *cell = if new_risk > 9 {
-                1 + new_risk % 10
-            } else {
-                new_risk
-            };
+            let mut cell_risk = bonus_risk as u32 + map[small_y][small_x];
+            while cell_risk > 9 {
+                cell_risk -= 9;
+            }
+            *cell = cell_risk;
         }
     }
     full_map
